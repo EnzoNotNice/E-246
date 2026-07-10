8479964df98797eb40f06e9eaf4d7e685d2564d1
Enzo <tryhard14090@gmail.com>
2026-07-10 18:16:56 +0000

    fix proxy error

--- dashboard/server.js
+++ dashboard/server.js
@@ -19,6 +19,7 @@ module.exports = (client) => {
     }
 
     const app = express();
+    app.set('trust proxy', 1);
     const PORT = process.env.PORT || 3000;
 
     passport.serializeUser((user, done) => done(null, user));
